e5c83bc48cde004a9c59b25c2cf69a41707163cb
jedigene@gmail.com <jedigene@gmail.com>
2015-06-05 16:29:30 -0700

    creates customer object with all pizza details ready to be returned with price.

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,21 @@ Customer.prototype.price = function() {
   this.pizzaPrice += total;
   return ('$' + this.pizzaPrice);
 };
+
+$(document).ready(function() {
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#input-name").val();
+    var customer = new Customer(name);
+    $(".new-pizza").each(function() {
+      var inputTopping = $(this).find("input.input-topping").val();
+      var inputSize = $(this).find(parseInt("input.input-size")).val();
+      var inputQuantity = $(this).find(parseInt("input.input-quantity")).val();
+      var pizza = (inputQuantity, inputTopping, inputSize);
+      var order = customer.pizza(pizza);
+      var price = customer.price();
+      $(".price").html('<h2>' + price + '</h2>');
+    });
+    debugger;
+  });
+});
