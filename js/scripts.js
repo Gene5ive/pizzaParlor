87bb05ee4a76de45befb9c7a924ed80c01f1e8d4
jedigene@gmail.com <jedigene@gmail.com>
2015-06-05 17:00:46 -0700

    be almost finished. meets most requirements.

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,20 @@ function Customer(name) {
   this.pizzaQuantity = null;
   this.pizzaTopping = null;
   this.pizzaSize = null;
-  this.pizzaPrice = '$';
-};
+  this.pizzaPrice = 0;
+}
 
 Customer.prototype.pizza = function(quantity, topping, size) {
   this.pizzaQuantity = quantity;
-  this.pizzaTopping = topping
-  this.pizzaSize = size
+  this.pizzaTopping = topping;
+  this.pizzaSize = size;
   return (this.pizzaQuantity + ' ' + this.pizzaSize + '-inch ' + this.pizzaTopping + ' pizzas for ' + this.name);
 };
 
 Customer.prototype.price = function() {
   var total = (this.pizzaQuantity * this.pizzaSize);
   this.pizzaPrice += total;
-  return ('$' + this.pizzaPrice);
+  return (this.pizzaPrice);
 };
 
 $(document).ready(function() {
@@ -24,15 +24,12 @@ $(document).ready(function() {
     event.preventDefault();
     var name = $("input#input-name").val();
     var customer = new Customer(name);
-    $(".new-pizza").each(function() {
-      var inputTopping = $(this).find("input.input-topping").val();
-      var inputSize = $(this).find(parseInt("input.input-size")).val();
-      var inputQuantity = $(this).find(parseInt("input.input-quantity")).val();
-      var pizza = (inputQuantity, inputTopping, inputSize);
-      var order = customer.pizza(pizza);
-      var price = customer.price();
-      $(".price").html('<h2>' + price + '</h2>');
-    });
+    var topping = $("input#input-topping").val();
+    var size = parseInt($("input#input-size").val());
+    var quantity = parseInt($("input#input-quantity").val());
+    customer.pizza(quantity, topping, size);
+    var price = customer.price();
+    $(".price").html('<h2>' + price + '</h2>');
     debugger;
   });
 });
